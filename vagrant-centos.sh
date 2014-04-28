c30649fd3cea790e4be2750d42ba3586597ffe9c
Tadas Sasnauskas <tadas@yoyo.lt>
2014-04-28 21:44:05 +0100

    I suppose it's a matter of taste?

--- vagrant-centos.sh
+++ vagrant-centos.sh
@@ -1,60 +1,61 @@
 #!/bin/bash
 
-vc_osiso=`find ./iso/CentOS* | head -n1`
-vc_guestiso="./iso/VBoxGuestAdditions.iso"
+UNIQ=`date | sha1sum | cut -c 1-7`
+ISO=`find ./iso/CentOS* | head -n1`
+GUEST_ISO="./iso/VBoxGuestAdditions.iso"
 
-vc_vboxbase="${HOME}/VirtualBox VMs"
-vc_basebox="CentOS-`echo ${vc_osiso} | cut -d "-" -f 2-3`"
-vc_hddbase="${vc_vboxbase}/${vc_basebox}/${vc_basebox}"
-vc_hddfile="${vc_hddbase}.vdi"
+VMS_ROOT="${HOME}/VirtualBox VMs"
+VM="CentOS-`echo ${ISO} | cut -d "-" -f 2-3`-${UNIQ}"
+VM_HDD="${VMS_ROOT}/${VM}/${VM}"
+VM_HDD_FILE="${VM_HDD}.vdi"
 
 checkdir() { [[ ! -d "$1" ]] && { echo "ERROR: missing $1 dir!"; exit 1; } }
 checkiso() { [[ ! -e "$1" ]] && { echo "ERROR: missing $1 file!"; exit 1; } }
 
 checkdir "$HOME"
-checkdir "$vc_vboxbase"
-checkiso "$vc_osiso"
-checkiso "$vc_guestiso"
+checkdir "$VMS_ROOT"
+checkiso "$ISO"
+checkiso "$GUEST_ISO"
 
-echo "ISO: ${vc_osiso}"
-echo "Machine name: ${vc_basebox}"
+echo "ISO: ${ISO}"
+echo "Machine name: ${VM}"
 
 mkdir "./tmp/"
-mkdir "./tmp/${vc_basebox}"
-7z x -o"./tmp/${vc_basebox}" "${vc_osiso}"
+mkdir "./tmp/${VM}"
+7z x -o"./tmp/${VM}" "${ISO}"
 
-cp ./vagrant-centos-min.ks ./tmp/${vc_basebox}/ks.cfg
-cp ./isolinux.cfg ./tmp/${vc_basebox}/isolinux/isolinux.cfg
+cp ./vagrant-centos-min.ks ./tmp/${VM}/ks.cfg
+cp ./isolinux.cfg ./tmp/${VM}/isolinux/isolinux.cfg
 
-mkisofs -o ./${vc_basebox}-ks.iso \
+mkisofs -o ./${VM}-ks.iso \
         -b isolinux/isolinux.bin \
         -c isolinux/boot.cat \
         -no-emul-boot \
         -boot-load-size 4 \
         -boot-info-table \
         -J -R \
-        -V "${vc_basebox}" \
-        ./tmp/${vc_basebox}/
+        -V "${VM}" \
+        ./tmp/${VM}/
 
 rm -rf ./tmp/
 
-vc_osiso="./${vc_basebox}-ks.iso"
+ISO="./${VM}-ks.iso"
 
 VBoxManage -v &> /dev/null || { echo "ERROR: VBoxManage not in path!"; exit 1; }
 
-VBoxManage createvm --name "$vc_basebox" --register
-VBoxManage modifyvm "$vc_basebox" --ostype RedHat_64 --memory 1024 --vram 12 --rtcuseutc on --ioapic on
-VBoxManage storagectl "$vc_basebox" --name ide0 --add ide
-VBoxManage storageattach "$vc_basebox" --storagectl ide0 --device 0 --port 0 --type dvddrive --medium "$vc_osiso"
-VBoxManage storageattach "$vc_basebox" --storagectl ide0 --device 0 --port 1 --type dvddrive --medium "$vc_guestiso"
-VBoxManage storagectl "$vc_basebox" --name sata0 --add sata --portcount 1
-VBoxManage createhd --filename "$vc_hddfile" --size 40960
-VBoxManage storageattach "$vc_basebox" --storagectl sata0 --port 0 --type hdd --medium "$vc_hddfile"
-VBoxManage modifyvm "$vc_basebox" --nic1 nat
-VBoxManage startvm "$vc_basebox"
+VBoxManage createvm --name "$VM" --register
+VBoxManage modifyvm "$VM" --ostype RedHat_64 --memory 1024 --vram 12 --rtcuseutc on --ioapic on
+VBoxManage storagectl "$VM" --name ide0 --add ide
+VBoxManage storageattach "$VM" --storagectl ide0 --device 0 --port 0 --type dvddrive --medium "$ISO"
+VBoxManage storageattach "$VM" --storagectl ide0 --device 0 --port 1 --type dvddrive --medium "$GUEST_ISO"
+VBoxManage storagectl "$VM" --name sata0 --add sata --portcount 1
+VBoxManage createhd --filename "$VM_HDD_FILE" --size 40960
+VBoxManage storageattach "$VM" --storagectl sata0 --port 0 --type hdd --medium "$VM_HDD_FILE"
+VBoxManage modifyvm "$VM" --nic1 nat
+VBoxManage startvm "$VM"
 
 echo "Press ENTER when installation is complete and box has powered off"; read
 
-vagrant package --base "$vc_basebox" --output "${vc_basebox}.box"
+vagrant package --base "$VM" --output "${VM}.box"
 
-VBoxManage unregistervm "$vc_basebox" --delete
+VBoxManage unregistervm "$VM" --delete
